a846ea46ea1f035ae1805586721878aeeee36455
Shen Li <cs.shenli@gmail.com>
2020-09-20 19:31:56 -0700

    Let rpc._all_gather use default RPC timeout
    `_all_gather` was converted from `_wait_all_workers` and inherited its
    5 seconds fixed timeout. As `_all_gather` meant to support a broader
    set of use cases, the timeout configuration should be more flexible.
    This PR makes `rpc._all_gather` use the global default RPC timeout.
    
    ghstack-source-id: ba521eb0495fe2a1a38e89c71b7a0490546fb45f
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/44983

--- torch/distributed/rpc/api.py
+++ torch/distributed/rpc/api.py
@@ -25,6 +25,7 @@ from . import (
     _is_current_rpc_agent_set,
     _reset_current_rpc_agent,
     _set_and_start_rpc_agent,
+    _set_rpc_timeout,
 )
 
 from .internal import (
@@ -34,7 +35,7 @@ from .internal import (
     _build_rpc_profiling_key,
 )
 
-from .constants import UNSET_RPC_TIMEOUT
+from .constants import DEFAULT_SHUTDOWN_TIMEOUT, UNSET_RPC_TIMEOUT
 
 
 logger = logging.getLogger(__name__)
@@ -163,8 +164,6 @@ def _all_gather(obj):
         _all_gather_sequence_id += 1
 
     is_leader = leader_name == self_name
-    # Set a long enough timeout for all shutdown messages to be processed.
-    timeout = 5  # second
 
     # Phase 1: Followers send it's object to the leader
     if is_leader:
@@ -173,8 +172,7 @@ def _all_gather(obj):
         rpc_sync(
             leader_name,
             _gather_to_leader,
-            args=(sequence_id, self_name, obj),
-            timeout=timeout,
+            args=(sequence_id, self_name, obj)
         )
 
     with _all_gather_dict_lock:
@@ -192,19 +190,23 @@ def _all_gather(obj):
             fut = rpc_async(
                 follower_name,
                 _broadcast_to_followers,
-                args=(sequence_id, states.gathered_objects),
-                timeout=timeout
+                args=(sequence_id, states.gathered_objects)
             )
             worker_name_to_response_future_dict[follower_name] = fut
+
+        failed_followers = []
         for follower_name, fut in worker_name_to_response_future_dict.items():
             try:
                 fut.wait()
             except RuntimeError as ex:
-                logger.error(
-                    "{worker_name} failed to respond to 'Shutdown Proceed.' request in {timeout}".format(
-                        worker_name=follower_name, timeout=timeout
-                    )
-                )
+                failed_followers.append(follower_name)
+
+        if failed_followers:
+            timeout = _get_current_rpc_agent()._get_timeout()
+            raise RuntimeError(
+                f"Followers {failed_followers} timed out after {timeout:.2f} seconds"
+            )
+
     return states.gathered_objects
 
 
@@ -217,7 +219,10 @@ def _wait_all_workers():
     terminate the RPC framework, and there is no guarantee that the RPC
     framework will work after this method returns.
     """
-    _all_gather(None)
+    try:
+        _all_gather(None)
+    except RuntimeError as ex:
+        logger.error(str(ex))
 
 
 @_require_initialized
@@ -269,6 +274,7 @@ def shutdown(graceful=True):
         >>> # wait for worker 0 to finish work, and then shutdown.
         >>> rpc.shutdown()
     """
+    _set_rpc_timeout(DEFAULT_SHUTDOWN_TIMEOUT)
     if graceful:
         _wait_all_workers()
         _delete_all_user_and_unforked_owner_rrefs()
